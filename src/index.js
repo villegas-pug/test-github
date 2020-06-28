359896ef4ce8b2b3adfcc14b53db2da5a886ef08
rguevarav <villegas.pug@gmail.com>
2020-06-27 22:40:46 -0500

    index.js actualizado!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Debounce from './components/debounce'
+/* import Debounce from './components/debounce' */
 
 ReactDOM.render(
   <>
     <h1>Hola este es un sitio de prueba!!!</h1>
+    <h2>Se actualizó el sitio de prueba!!!</h2>
   </>,
   document.getElementById('root')
 )
