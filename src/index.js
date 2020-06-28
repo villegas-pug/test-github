f98e1bb462054b79080fb96db24d1b669d06122f
rguevarav <villegas.pug@gmail.com>
2020-06-27 21:47:18 -0500

    Add directory /build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Debounce from './components/debounce'
 
 ReactDOM.render(
   <>
-    <h1>Hola mundo!!!</h1>
+    <h1>Hola este es un sitio de prueba!!!</h1>
   </>,
   document.getElementById('root')
 )
